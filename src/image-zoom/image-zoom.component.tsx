7720df0b49c2856af4d07df412f4cb2436d93531
Artur Szady <artur.szady@gmail.com>
2023-05-18 22:20:54 +0200

    remove ts-check

--- src/image-zoom/image-zoom.component.tsx
+++ src/image-zoom/image-zoom.component.tsx
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import * as React from 'react';
 import { Animated, LayoutChangeEvent, PanResponder, StyleSheet, View } from 'react-native';
 import styles from './image-zoom.style';
